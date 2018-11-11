d98a63b76a180c30204ac850b683e3cfa6758e30
Alejandro Nanez <alejonanez@gmail.com>
2018-11-11 16:53:13 -0500

    More progress with TextClient

---
+++ text_client/lib/text_client/player.ex
@@ -0,0 +1,51 @@
+defmodule TextClient.Player do
+  alias TextClient.State
+
+  # won, lost, good_guess, bad guess, alreadu used, initialising
+  def play(%State{tally: %{game_state: :won}}) do
+    exit_with_message("You WON!")
+  end
+
+  def play(%State{tally: %{game_state: :lost}}) do
+    exit_with_message("Sorry, you lost")
+  end
+
+  def play(game = %State{tally: %{game_state: :good_guess}}) do
+    continue_with_message(game, "Good guess!")
+  end
+
+  def play(game = %State{tally: %{game_state: :bad_guess}}) do
+    continue_with_message(game, "Sorry, that isn't in the word")
+  end
+
+  def play(game = %State{tally: %{game_state: :already_used}}) do
+    continue_with_message(game, "You've already used that letter")
+  end
+
+  def play(game) do
+    continue(game)
+  end
+
+  defp continue_with_message(game, msg) do
+    IO.puts(msg)
+    continue(game)
+  end
+
+  def continue(game) do
+    game
+    |> display()
+    |> prompt()
+    |> make_move()
+    |> play()
+  end
+
+  def display(game), do: game
+  def prompt(game), do: game
+  def make_move(game), do: game
+
+  defp exit_with_message(msg) do
+    IO.puts(msg)
+    # Elixir, please, don't recover after exit
+    exit(:normal)
+  end
+end
